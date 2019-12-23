ffe8da8aac076d78ad098d85619056c3a003998e
Eder Ramirez <eder.ramirez87@gmail.com>
2019-12-23 09:45:17 -0600

    displaying quote

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+function Frase({frase}) {
+  return (
+    <div className="frase">
+      <h1>{frase.quote}</h1>
+      <p>- {frase.author}</p>
+    </div>
+  )
+}
+
 function App() {
   const [frase, obtenerFrase] = useState({});
 
@@ -16,10 +25,14 @@ function App() {
       consultarAPI()
     }, []
   );
-
-  console.log(frase)
   
-  return <p>Hola</p>
+  return (
+    <div className="contenedor">
+      <Frase
+        frase={frase}
+      />
+    </div>
+  )
 }
 
 export default App;
