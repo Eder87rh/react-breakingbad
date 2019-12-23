ec27e19b63d27089632eeafa8470b8ce164d96b0
Eder Ramirez <eder.ramirez87@gmail.com>
2019-12-23 09:41:04 -0600

    consuming the API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [frase, obtenerFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    // Agregar el resultado de la api al state
+    obtenerFrase(resultado.data[0]);
+  }
+
+  // Consulta a una rest api
+  useEffect(
+    () =>  { 
+      consultarAPI()
+    }, []
   );
+
+  console.log(frase)
+  
+  return <p>Hola</p>
 }
 
 export default App;
